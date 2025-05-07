2196a1f9bb52adea6c6918a994249c2e8209219a
Yannick Rd <yannick.rd@tuta.io>
2025-05-07 18:44:20 +0700

    Correct ex04

--- ex04/args_errors.cpp
+++ ex04/args_errors.cpp
@@ -9,7 +9,7 @@ void    exit_if_args_errors(int argc, char* argv[])
         std::cerr << "Error: invalid number of arguments" << std::endl;
         exit (1);
     }
-    if (!argv[1] || !argv[2] || argv[3])
+    if (!argv[1] || !argv[2] || !argv[3])
     {
         std::cerr << "Error: at least one argument is null" << std::endl;
         exit (2);
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char* argv[])
     exit_if_args_errors(argc, argv);
     std::string     file_name = argv[1];
     std::string     s1 = argv[2];
+    if (s1.empty()) {
+        std::cerr << "Error: search string cannot be empty" << std::endl;
+        return 1;
+    }
     std::string     s2 = argv[3];
     std::ifstream   ifs(file_name.c_str());
     if (!ifs.is_open())
@@ -29,7 +33,14 @@ int main(int argc, char* argv[])
         temp_buffer.erase(pos, s1.length());
         temp_buffer.insert(pos, s2);
     }
-    std::ofstream   ofs(file_name.c_str()); 
+    std::string outFilename = file_name + ".replace";
+    std::ofstream   ofs(outFilename.c_str());
+    if (!ofs.is_open())
+    {
+        std::cerr   << "Error: Could not create output file " 
+                    << outFilename << std::endl;
+        return (4);
+    }
     ofs << temp_buffer;
     ofs.close();
     return (0);
