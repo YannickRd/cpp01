890b5a200ffaccdfd3ceddd377e30047829d8c7c
Yannick Rd <yannick.rd@tuta.io>
2025-05-16 13:44:56 +0700

    Refactore ex01 main for smaller function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,41 +2,43 @@
 
 #include "main.hpp"
 
+void	output_test_log(int i, int horde_size, std::string horde_name)
+{
+	std::cout << "=======================TEST " << i << "======================"
+			  << std::endl;
+	std::cout << "... calling zombieHorde() with parameters: "
+			  << horde_size << ", " << horde_name << " ..." << std::endl;
+}
+
+void	assign_annouce_free(Zombie *MyHorde, int size, std::string name)
+{
+	MyHorde = zombieHorde(size, name);
+	for (int i = 0; i < size; i++)
+		MyHorde[i].announce();
+	delete[] MyHorde;
+}
+
 int main(void)
 {
-	Zombie *MyHorde;
+	Zombie *MyHorde = NULL;
 	int horde_size;
 	std::string horde_name;
+	int i = 1;
 
 	horde_size = 9;
 	horde_name = "Joe";
-	std::cout << "===========================TEST 1=========================="
-			  << std::endl;
-	std::cout << "... calling zombieHorde() with parameters: "
-			  << horde_size << ", " << horde_name << " ..." << std::endl;
-	MyHorde = zombieHorde(horde_size, horde_name);
-	for (int i = 0; i < horde_size; i++)
-		MyHorde[i].announce();
-	delete[] MyHorde;
-	std::cout << "===========================TEST 2=========================="
-			  << std::endl;
+	output_test_log(i++, horde_size, horde_name);
+	assign_annouce_free(MyHorde, horde_size, horde_name);
+
 	horde_size = 1;
 	horde_name = "John";
-	std::cout << "... calling zombieHorde() with parameters: "
-			  << horde_size << ", " << horde_name << " ..." << std::endl;
-	MyHorde = zombieHorde(horde_size, horde_name);
-	for (int i = 0; i < horde_size; i++)
-		MyHorde[i].announce();
-	delete[] MyHorde;
-	std::cout << "===========================TEST 3=========================="
-			  << std::endl;
+	output_test_log(i++, horde_size, horde_name);
+	assign_annouce_free(MyHorde, horde_size, horde_name);
+
 	horde_size = 0;
 	horde_name = "nobody";
-	std::cout << "... calling zombieHorde() with parameters: "
-			  << horde_size << ", " << horde_name << " ..." << std::endl;
-	MyHorde = zombieHorde(horde_size, horde_name);
-	for (int i = 0; i < horde_size; i++)
-		MyHorde[i].announce();
-	delete[] MyHorde;
+	output_test_log(i++, horde_size, horde_name);
+	assign_annouce_free(MyHorde, horde_size, horde_name);
+
 	return (0);
 }
